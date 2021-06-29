1f4e5382906d188a3794fef9d1fd8485fe87d306
Yunosuke Takahashi <takayu112233aa@gmail.com>
2021-06-29 11:14:22 +0900

    20210629_02 直樹くん追加

---
+++ public/register.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+require_once '../classes/UserLogic.php';
+
+$err = [];
+
+$token = filter_input(INPUT_POST, 'csrf_token');
+//トークンがない、もしくは一致しない場合、処理を中止
+if (!isset($_SESSION['csrf_token']) || $token !== $_SESSION['csrf_token']) {
+  exit('不正なリクエスト');
+}
+
+unset($_SESSION['csrf_token']);
+
+if(!$username = filter_input(INPUT_POST, 'username')){
+    $err[] ='ユーザ名を記入してください';
+}
+if(!$nickname = filter_input(INPUT_POST, 'nickname')){
+    $err[] ='ニックネームを記入してください';
+}
+$pass = filter_input(INPUT_POST, 'pass');
+//正規表現
+if (!preg_match("/\A[a-z\d]{8,100}+\z/i",$pass)){
+    $err[] = 'パスワードは英数字8文字以上100文字以下にしてください。';
+}
+$password_conf = filter_input(INPUT_POST, 'password_conf');
+if ($pass !== $password_conf){
+    $err[] = '確認用パスワードど異なってます';
+}
+
+if (count($err) === 0){
+    //ユーザを登録する処理
+    $hasCreated = UserLogic::createUser($_POST);
+
+    if(!$hasCreated){
+        $err[] = '登録に失敗しました';
+    }
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザ登録完了画面</title>
+</head>
+<body>
+<?php if (count($err) > 0) : ?>
+    <?php foreach($err as $e) : ?>
+        <p><?php echo $e ?></p>
+    <?php endforeach ?>
+    <?php else : ?>
+    <p>ユーザ登録が完了しました</p>
+    <?php endif ?>
+    <a href="./signup_form.php">戻る</a>
+</body>
+</html>
---
+++ public/signup_form.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+require_once '../function.php';
+require_once '../classes/UserLogic.php';
+
+$result = UserLogic::checkLogin();
+if($result) {
+    header('Location: mypage.php');
+    return;
+}
+$login_err = isset($_SESSION['login_err']) ? $_SESSION['login_err'] : null;
+unset($_SESSION['login_err']);
+//isset(変数)で変数に値が入っているか確認し、3項演算子で比較
+//$_SESSION['login_err']を評価し中身があるなら$login_errに代入、ないのならばnullを代入
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザ登録画面</title>
+</head>
+<body>
+    <h2>ユーザ登録フォーム</h2>
+        <?php if (isset($login_err)) : ?>
+            <p><?php echo $login_err; ?></p>
+        <?php endif; ?>
+    <form action="register.php" method="POST">
+    <p>
+        <label for="username">ユーザ名:</label>
+        <input type="text" name="username">
+    </p>
+    <p>
+        <label for="nickname">ニックネーム:</label>
+        <input type="nickname" name="nickname">
+    </p>
+    <p>
+        <label for="password">パスワード:</label>
+        <input type="password" name="pass">
+    </p>
+    <p>
+        <label for="password_conf">パスワード確認:</label>
+        <input type="password" name="password_conf">
+    </p>
+    <input type="hidden" name="csrf_token" value="<?php echo h(setToken()); ?>">
+    <!-- -->
+    <p>
+        <input type="submit" value="新規登録">
+    </p>
+    </form>
+    <a href="/public/login_form.php">ログインする</a>
+</body>
+</html>
